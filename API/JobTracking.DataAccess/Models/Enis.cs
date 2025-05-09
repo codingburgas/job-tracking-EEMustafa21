b3d1e1d310872741134827e2b35948469701f372
EEMustafa21 <EEMustafa21@codingburgas.bg>
2025-05-09 09:58:00 +0300

    Add models

---
+++ API/JobTracking.DataAccess/Models/Enis.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using JobTracking.DataAccess.Data.Base;
+
+namespace JobTracking.DataAccess.Models;
+
+public class Enis : IEntity
+{
+    [Key]
+    public int Id { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime CreatedOn { get; set; }
+
+    [Required]
+    public string CreatedBy { get; set; }
+    public DateTime? UpdatedOn { get; set; }
+    public string? UpdatedBy { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime StartDate { get; set; }
+
+    [Required]
+    public int DenisID { get; set; }
+    public virtual Denis denis { get; set; }
+
+}
